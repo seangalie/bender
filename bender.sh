e20ab978ec8d2232fe07f5cfafcbe7dea713233b
Sean Galie <galiean251@gmail.com>
2024-12-11 12:53:41 -0500

    Create script placeholder file

---
+++ bender.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# /---------------------------------------------\
+# |                   BENDER                    |
+# +---------------------------------------------+
+# | A small Debian setup script for Desktop use |
+# \---------------------------------------------/
+#     Sean Galie (https://www.seangalie.com/)
+
+echo "Hello world.  I'm a placeholder for a local script."
